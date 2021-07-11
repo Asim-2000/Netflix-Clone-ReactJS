ef32eeb9a99ce678b873061042896411f12a1bf4
Muhammad Asim Khaskheli <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-07-12 00:20:07 +0500

    📦 NEW: initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_IMiOtblHW9zNObl2fikiMk_660cXHnw",
+  authDomain: "netflix-clone-2bf48.firebaseapp.com",
+  projectId: "netflix-clone-2bf48",
+  storageBucket: "netflix-clone-2bf48.appspot.com",
+  messagingSenderId: "769891008842",
+  appId: "1:769891008842:web:ab4d9176cbbeecbabc41b3",
+}
